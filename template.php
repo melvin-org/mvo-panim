2ccf786d0c524bb075858e1fad3ea6b70ee6fe32
Melvin Hoh <cheekinhoh@gmail.com>
2021-06-23 13:04:28 +0800

    Revert "delete"
    This reverts commit 7885061d7fad1537ff6c51d3e8a06b8729aa05fc.

---
+++ template.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<title>Poggo Animation</title>
+<head>
+<link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+
+<!--Get header -->
+<?php
+include 'header.php'; 
+?>
+
+<!-- page wrapper class -->
+<div class="page-wrapper">
+
+<!-- START BELOW HERE -->
+
+
+
+
+
+
+
+
+
+
+<!-- END HERE -->
+</div>
+
+
+
+<!--Get footer -->
+<?php
+include 'footer.php';
+?>
+
+</body>
+</html>
