f16032965dc288df15fad10b5e421dd9d1080262
leonjensentan <leonjensentan@gmail.com>
2021-06-23 13:00:23 +0800

    Header Footer Style.css

--- footer.php
+++ footer.php
@@ -9,53 +9,44 @@
 
 
 <div class="footer">
-	<!-- <div class="footer-content">
+	<div class="footer-content">
 	
 	<div class="footer-section social">
-		<h4><strong>Pestle & Mortar Clothing</strong></h4>
-		<p>Est. in the year 2010, Pestle & Mortar Clothing isn’t just about what you wear, but also a projection of our personal lifestyles, culture and homeland. We are mainly designed around the things we see familiar growing up in Southeast Asia.</p>
+		<h4><strong>Poggo Animation</strong></h4>
+		<p>Poggo Animation Sdn Bhd is a company that creates original animated contents.</p>
+		<p>Our studio is located in Cyberjaya, Malaysia</p>
 		<div class="socials">
 		<a href="#"><i class="fab fa-facebook"></i></a>
 		<a href="#"><i class="fab fa-instagram"></i></a>
-		<a href="#"><i class="fab fa-pinterest"></i></a>
 		<a href="#"><i class="fab fa-youtube"></i></a>
 		</div>
 	</div>
 	<div class="footer-section about">
 		<h4>About Us</h4>
 		<ul class="footer-about" style="padding-inline-start: 0px;">
-			<li><a href="#">Our Brand</a></li>
-			<li><a href="#">Careers</a></li>
-			<li><a href="#">Storefronts</a></li>
-			<li><a href="#">Stockists</a></li>
-			<li><a href="#">Homie Rewards</a></li>
-			<li><a href="#">Whatsapp Order</a></li>
-			<li><a href="#">Contact Us</a></li>
-			<li><a href="#">Terms of Use</a></li>
+			<li><a href="#">Shipping Policy</a></li>
+			<li><a href="#">Refund Policy</a></li>
+			<li><a href="#">Terms of Service</a></li>
+			<li><a href="#">Privacy Policy</a></li>
 		</ul>
 	</div>
 	
 	<div class="footer-section help">
-	<h4>Help</h4>
+	<h4>Contact Us</h4>
 		<ul class="footer-about"  style="padding-inline-start: 0px;">
-			<li><a href="#">Orders</a></li>
-			<li><a href="#">Products</a></li>
-			<li><a href="#">Shipping/Tracking</a></li>
-			<li><a href="#">Payments</a></li>
-			<li><a href="#">Size Chart</a></li>
-			<li><a href="#">Returns</a></li>
-			<li><a href="#">Privacy Policy</a></li>
+			<li>If you have any questions, please <a href="#"><u>contact us</u></a></li>
 		</ul>
+
 	<h4>Payment</h4>
 	<ul id="footer-payment"  style="padding-inline-start: 0px;">
 			<li><i class="fab fa-cc-visa"></i></li>
 			<li><i class="fab fa-paypal"></i></li>
 			<li><i class="fab fa-cc-mastercard"></i></li>
 		</ul>
-	</div> -->
+	</div>
 	
 	<div class="footer-bottom">
-	  <h5 style="padding-left:25px;">Copyright&copy; 2020 Poggo Animation</h5>
+	  <h5 style="padding-left:25px;">&copy; Poggo Animation</h5>
 	 
 	</div>
 
