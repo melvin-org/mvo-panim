02e9427e3cdea584afc9e30d1c11b7846d126605
leonjensentan <leonjensentan@gmail.com>
2021-06-23 17:27:09 +0800

    created form to add products and images folder

---
+++ form.php
@@ -0,0 +1,12 @@
+<form method="POST" action="form2.php" enctype="multipart/form-data">
+
+<input type="text" id="pname" name="pname" placeholder="name" required>
+<input type="text" id="pcat" name="pcat" placeholder="cat" required>
+<input type="file" id="photo" name="photo" required>
+<input type="text" id="pprice" name="pprice" placeholder="price" required>
+<input type="text" id="pdesc" name="pdesc" placeholder="desc" required>
+<input type="text" id="pstatus" name="pstatus" placeholder="status" required>
+
+<button type="submit" name="add">Save</button>
+
+</form>
---
+++ form2.php
@@ -0,0 +1,42 @@
+<?php
+
+$con=mysqli_connect("localhost", "admin", null, "pganim");
+
+if(isset($_POST['add'])){
+    $pname = $_POST['pname'];
+    $pcat = $_POST['pcat'];
+    $pprice = $_POST['pprice'];
+    $pdesc = $_POST['pdesc'];
+    $pstatus = $_POST['pstatus'];
+    $filename = $_FILES['photo']['name'];
+    date_default_timezone_set("Asia/Kuala_Lumpur");
+    $createdAt =  date('Y-m-d H:i:s');
+    $updatedAt =  date('Y-m-d H:i:s');
+
+    $query1="select count(product_id) from products";
+    $result=mysqli_query($con, $query1);
+    while($row=mysqli_fetch_array($result)) {
+        $pid=$row["count(product_id)"] + 1;
+    }
+
+    if(!empty($filename)){
+        $ext = pathinfo($filename, PATHINFO_EXTENSION);
+        $new_filename = $pname.'.'.$ext;
+        move_uploaded_file($_FILES['photo']['tmp_name'], 'images/'.$new_filename);
+    }
+    else{
+        $new_filename = '';
+    }
+
+$query="INSERT INTO products values ('$pid', '$pname', '$pcat', '$new_filename','$pprice', '$pdesc', '$pstatus', '$createdAt','$updatedAt')";
+$result=mysqli_query($con, $query);
+
+if($result>0) echo "<p style='font-family:calibri;text-align:center;'><center>product successfully added.</center></p>";
+else echo "Error";
+
+mysqli_close($con);
+
+}
+
+
+?>
