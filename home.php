c198989b9fa959faa3b65cc87a43b3a790e03501
Melvin Hoh <cheekinhoh@gmail.com>
2021-06-24 18:18:27 +0800

    update font home

--- home.php
+++ home.php
@@ -13,43 +13,12 @@
 </head>
 
 <body>
-  <!--Navigation bar-->
-  <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light py-3 fixed-top">
-        <img src="img/logo1.png" alt="" height="45px">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-      
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ml-auto">
-
-            <li class="nav-item">
-              <a class="nav-link" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Mystery Sale</a>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link" href="#">Apparel</a>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link" href="#">Collection</a>
-              </li>
-
-              <li class="nav-item">
-                <i class="fas fa-user"></i>
-                <i class="fas fa-shopping-cart"></i>
-              </li>
-          </div>
-      </nav>  -->
 
   <?php
   include 'header.php';
   ?>
 
-  <div class="page-wrapper">
+  <div class="page-wrapper quicksand-font">
 
     <section id="home">
       <div class="container">
